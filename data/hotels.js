22f6839e82cd58f5e081535ccba083a437fcff2a
Morgan <121578632+morganharris39@users.noreply.github.com>
2025-03-26 16:52:19 -0600

    added json and grid template area

---
+++ data/hotels.js
@@ -0,0 +1,92 @@
+export const hotels = [
+      {
+        "name": "The Prancing Pony",
+        "image": "hobbit-home-1.webp",
+        "phone": "(801) 555-0123",
+        "address": [
+          "1 Bree Lane",
+          "Hobbiton, SH 84101"
+        ]
+      },
+      {
+        "name": "Bag End Boutique",
+        "image": "hobbit-home-2.webp",
+        "phone": "(801) 555-0124",
+        "address": [
+          "9 Bagshot Row",
+          "The Shire, SH 84060"
+        ]
+      },
+      {
+        "name": "Green Dragon Lodge",
+        "image": "hobbit-home-3.webp",
+        "phone": "(801) 555-0125",
+        "address": [
+          "15 Bywater Road",
+          "Hobbiton, SH 84028"
+        ]
+      },
+      {
+        "name": "Rivendell Rest",
+        "image": "hobbit-home-4.webp",
+        "phone": "(435) 555-0126",
+        "address": [
+          "100 Elven Valley",
+          "Imladris, SH 84770"
+        ]
+      },
+      {
+        "name": "Brandywine River",
+        "image": "hobbit-home-5.webp",
+        "phone": "(801) 555-0127",
+        "address": [
+          "25 River Bank",
+          "Bucklebury, SH 84111"
+        ]
+      },
+      {
+        "name": "Tookborough Manor",
+        "image": "hobbit-home-6.webp",
+        "phone": "(801) 555-0128",
+        "address": [
+          "3 Great Smials Way",
+          "Tuckborough, SH 84092"
+        ]
+      },
+      {
+        "name": "Mathom House",
+        "image": "hobbit-home-7.webp",
+        "phone": "(801) 555-0129",
+        "address": [
+          "7 Michel Delving Road",
+          "Westfarthing, SH 84321"
+        ]
+      },
+      {
+        "name": "Farmer's Inn",
+        "image": "hobbit-home-8.webp",
+        "phone": "(801) 555-0130",
+        "address": [
+          "12 Mushroom Lane",
+          "Marish, SH 84401"
+        ]
+      },
+      {
+        "name": "The Golden Perch",
+        "image": "hobbit-home-9.webp",
+        "phone": "(435) 555-0131",
+        "address": [
+          "45 Stock Road",
+          "Eastfarthing, SH 84532"
+        ]
+      },
+      {
+        "name": "Brandybuck Hall",
+        "image": "hobbit-home-10.webp",
+        "phone": "(801) 555-0132",
+        "address": [
+          "1 Brandy Hall",
+          "Buckland, SH 84150"
+        ]
+      }
+    ]
--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,35 @@ const pw = document.querySelector('#pageWrapper')
 hb.addEventListener('click', () => {
     pw.classList.toggle('moveOver')
 })
+
+// import hotels json file
+import { hotels } from '../data/hotels.js'
+
+const hotelCards = document.querySelector('#cards')
+
+// for loop for cards
+hotels.forEach(hotel => {
+    
+    const cardSection = document.createElement('section')
+
+    const cardImage = document.createElement('img')
+    cardImage.src = `images/${hotel.image}`
+    cardImage.alt = hotel.name
+
+    const cardName = document.createElement('h2')
+    cardName.textContent = hotel.name
+
+    const cardAddress = document.createElement('address')
+    cardAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`
+
+    const cardPhone = document.createElement('a')
+    cardPhone.text = hotel.phone
+    cardPhone.href = `tel:${hotel.phone}`
+
+    cardSection.appendChild(cardImage)
+    cardSection.appendChild(cardName)
+    cardSection.appendChild(cardAddress)
+    cardSection.appendChild(cardPhone)
+    hotelCards.appendChild(cardSection)
+
+});
